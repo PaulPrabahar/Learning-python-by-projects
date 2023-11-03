f2719547042dbbca637b43bba93429eb64a2cf07
PaulPrabahar <prabahar1080@gmail.com>
2023-11-03 14:31:48 +0530

    loop added for continues work

--- basic_calculator.py
+++ basic_calculator.py
@@ -24,37 +24,39 @@ def div(a, b):
     print(str(a) + "/" + str(b) + '=' + str(answer))
 
 
-print("A for Addition")
-print("B for Subraction")
-print("C for Multiplication")
-print("D for Division")
-print("E for Exit")
+while True:
+    print("A for Addition")
+    print("B for Subraction")
+    print("C for Multiplication")
+    print("D for Division")
+    print("E for Exit")
 
-choice = input("input your choice: ")
+    choice = input("input your choice: ")
 
-if choice == "a" or choice == "A":
-    print("Enter the values: ")
-    a = int(input("enter the first value: "))
-    b = int(input("enter the second value: "))
-    add(a, b)
+    if choice == "a" or choice == "A":
+        print("Enter the values: ")
+        a = int(input("enter the first value: "))
+        b = int(input("enter the second value: "))
+        add(a, b)
 
-elif choice == "b" or choice == "B":
-    print("Enter the values: ")
-    a = int(input("enter the first value: "))
-    b = int(input("enter the second value: "))
-    sub(a, b)
+    elif choice == "b" or choice == "B":
+        print("Enter the values: ")
+        a = int(input("enter the first value: "))
+        b = int(input("enter the second value: "))
+        sub(a, b)
 
-elif choice == "c" or choice == "C":
-    print("Enter the values: ")
-    a = int(input("enter the first value: "))
-    b = int(input("enter the second value: "))
-    mul(a, b)
+    elif choice == "c" or choice == "C":
+        print("Enter the values: ")
+        a = int(input("enter the first value: "))
+        b = int(input("enter the second value: "))
+        mul(a, b)
 
-elif choice == "d" or choice == "D":
-    print("Enter the values: ")
-    a = int(input("enter the first value: "))
-    b = int(input("enter the second value: "))
-    div(a, b)
+    elif choice == "d" or choice == "D":
+        print("Enter the values: ")
+        a = int(input("enter the first value: "))
+        b = int(input("enter the second value: "))
+        div(a, b)
 
-else:
-    print("Thanks for using the calculator")
+    else:
+        print("Thanks for using the calculator")
+        quit()
