bf3cd0b6f951930328e41dfd317293e58074177f
PaulPrabahar <prabahar1080@gmail.com>
2023-11-03 12:54:52 +0530

    code to replace a word in a paragraph

---
+++ word_replacement_program.py
@@ -0,0 +1,10 @@
+def replace_word():
+
+    str = "hi my name is paul, I am from tuty. yaaaaaa"
+    word_to_replace = input("enter the word to replace: ")
+    word_to_replacement = input("enter the word for replacement: ")
+
+    print(str.replace(word_to_replace, word_to_replacement))
+
+
+replace_word()
